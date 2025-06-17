cd56296a3919f9db7a257ec511567267637bea9b
faith <faith.henu@student.moringaschool.com>
2025-06-17 09:56:35 +0300

    added files

--- server/controllers/restaurant_pizza_controller.py
+++ server/controllers/restaurant_pizza_controller.py
@@ -14,18 +14,18 @@ def create_restaurant_pizza():
     pizza_id = data.get('pizza_id')
     restaurant_id = data.get('restaurant_id')
 
-    # Validate price
+    
     if price is None or price < 1 or price > 30:
         return jsonify({"errors": ["Price must be between 1 and 30"]}), 400
 
-    # Validate foreign keys
+   
     pizza = Pizza.query.get(pizza_id)
     restaurant = Restaurant.query.get(restaurant_id)
 
     if not pizza or not restaurant:
         return jsonify({"errors": ["Invalid pizza_id or restaurant_id"]}), 400
 
-    # Create and save
+    
     new_rp = RestaurantPizza(price=price, pizza_id=pizza_id, restaurant_id=restaurant_id)
     db.session.add(new_rp)
     db.session.commit()
